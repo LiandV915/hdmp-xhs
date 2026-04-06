f887b0ea2b625396482669d33678fbcb65a929c4
23995 <liyufan@bupt.edu.cn>
2026-04-06 19:47:30 +0800

    2321

---
+++ src/main/java/com/hmdp/dto/CheckoutDTO.java
@@ -0,0 +1,31 @@
+package com.hmdp.dto;
+
+public class CheckoutDTO {
+    private Long orderId;
+    private String address;
+    private String phone;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
